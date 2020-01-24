30834dd8174a687c2c208a61e9ae1e8665b78668
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-01-24 10:20:26 -0500

    set views directory to /templates

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@
 
 
 const publicPath = path.join(__dirname, '../public')
+const viewsPath  = path.join(__dirname, '../templates')
 
 app.use(express.static(publicPath))
+app.set('views', viewsPath)
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
