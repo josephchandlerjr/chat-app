d43af1fac3435d3ed9a74a94b81b78bb873760b9
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-01-24 10:27:53 -0500

    serve up ejs file to start

--- src/index.js
+++ src/index.js
@@ -10,5 +10,10 @@ const viewsPath  = path.join(__dirname, '../templates')
 app.use(express.static(publicPath))
 app.set('views', viewsPath)
 
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+	res.render('index')
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
