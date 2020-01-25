76def00deeb6d13c1ac282d2298762571e738427
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-01-25 12:28:13 -0500

    pass server to socket.io

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@
  	   app		= express(),
  	   path		= require('path'),
  	   http		= require('http'),
+ 	   socketio	= require('socket.io'),
  	   port		= process.env.PORT || 3000
 
 
 const server = http.createServer(app)
+
+const io = socketio(server)
 
 const publicPath = path.join(__dirname, '../public')
 const viewsPath  = path.join(__dirname, '../templates')
