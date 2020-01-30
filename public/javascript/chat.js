3d679dc575de261f48bdde4125b80cbdb9921f8f
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-01-30 08:47:18 -0500

    get latitude and longitude by calling navigator.poosition, the browser geolocation api

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -12,3 +12,14 @@ document.querySelector('#message-form').addEventListener('submit', (evt) => {
 	const clientMessageText = evt.target.elements.message // use name of input field to accessed it on elements obj
  	socket.emit('sendMessage', clientMessageText.value)
 })
+
+document.querySelector('#send-location').addEventListener('click', () => {
+	if (!navigator.geolocation) {
+		return alert('Geolocation is not supported by your browser')
+	}
+
+	navigator.geolocation.getCurrentPosition( (position) => {  //asyncronous but currently doesn't support promises
+		let {latitude, longitude} = position.coords
+		console.log(latitude, longitude)
+	}) 
+})
