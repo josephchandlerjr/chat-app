cd658a72acaec1c39fb0db9625e8c06591c92721
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-01-30 11:44:44 -0500

    diabled send location button while location is being retrieve. Re-enable button in event acknowledgement.

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -5,6 +5,8 @@ const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
 	console.log(message)
 })
@@ -15,30 +17,38 @@ $messageForm.addEventListener('submit', (evt) => {
 	evt.preventDefault()
 	// disable
 	$messageFormButton.setAttribute('disabled', 'disabled')
-	//clear input form and focus on it
-	$messageFormInput.value = ''
-	$messageFormInput.focus()
 
 	const clientMessageText = evt.target.elements.message // use name of input field to accessed it on elements obj
+
+
  	socket.emit('sendMessage', clientMessageText.value, (error) => {
+ 		//clear input form and focus on it
+		$messageFormInput.value = ''
+		$messageFormInput.focus()
+
  		if (error) {
  			console.log(error)
  		}
 
- 		//enable
+ 		//re-enable
  		$messageFormButton.removeAttribute('disabled')
 
 
  	})
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
+	$sendLocationButton.setAttribute('disabled', 'disabled')
 	if (!navigator.geolocation) {
 		return alert('Geolocation is not supported by your browser')
 	}
 
 	navigator.geolocation.getCurrentPosition( (position) => {  //asyncronous but currently doesn't support promises
         let {latitude, longitude} = position.coords
-		socket.emit('sendLocation', {latitude, longitude}, (msg) => {if (msg) console.log(msg)})
+		socket.emit('sendLocation', {latitude, longitude}, (msg) => {
+			if (msg) console.log(msg)
+			$sendLocationButton.removeAttribute('disabled')
+
+		})
 	}) 
 })
